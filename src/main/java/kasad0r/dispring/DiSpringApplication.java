3cb571753cb33bc64075495a075a917539284e01
kasad0r <2431082dima>
2020-07-13 14:18:29 +0200

    Simple context example

--- src/main/java/kasad0r/dispring/DiSpringApplication.java
+++ src/main/java/kasad0r/dispring/DiSpringApplication.java
@@ -1,13 +1,22 @@
 package kasad0r.dispring;
 
+import kasad0r.dispring.cotrollers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(DiSpringApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+
+        String greeting = controller.sayHello();
+
+        System.out.println(greeting);
+    }
 
 }
---
+++ src/main/java/kasad0r/dispring/cotrollers/MyController.java
@@ -0,0 +1,12 @@
+package kasad0r.dispring.cotrollers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello world!!");
+        return "Hi Folks!";
+    }
+}
